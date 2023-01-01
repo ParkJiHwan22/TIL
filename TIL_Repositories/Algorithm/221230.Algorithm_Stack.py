6a44d89ca6d9d0711106460271a245e1edabdb1c
ParkJiHwan22 <ParkJiHwan22@github.com>
2023-01-01 19:45:39 +0900

    Add Algorithm and IT_Knowledge

---
+++ TIL_Repositories/Algorithm/221230.Algorithm_Stack.py
@@ -0,0 +1,63 @@
+### 스택 구현 예제 (Python)
+# stack = []
+
+# # 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+# stack.append(5)
+# stack.append(2)
+# stack.append(3)
+# stack.append(7)
+# stack.pop()
+# stack.append(1)
+# stack.append(4)
+# stack.pop()
+
+# print(stack[::-1]) # 최상단 원소부터 출력
+# print(stack) # 최하단 원소부터 출력
+
+# ### 스택 구현 예제 (Python)
+# from collections import deque
+
+# # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+# queue = deque()
+
+# # # 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+
+# queue.append(5)
+# queue.append(2)
+# queue.append(3)
+# queue.append(7)
+# queue.popleft()
+# queue.append(1)
+# queue.append(4)
+# queue.popleft()
+
+# print(queue) # 먼저 들어온 순서대로 출력
+# queue.reverse() # 역순으로 바꾸기
+# print(queue) # 나중에 들어온 순서대로 출력
+
+### 우선순위 큐 라이브러리를 활용한 힙 구현 예제 (Python)
+import sys
+import heapq
+input = sys.stdin.readline
+
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq heappush(h, value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+res = heapsort(arr)
+
+for i in range(n):
+    print(res[i])
